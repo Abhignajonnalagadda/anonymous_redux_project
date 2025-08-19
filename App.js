86b2a5860e8bb6d7723768f14510d012e2384ecc
Abhignajonnalagadda <abhignajonnalagadda2580@gmail.com>
2025-08-19 09:15:40 +0530

    chore: build react application using redux

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Product from "./components/Product";
+import products from "./store/productList";
+
+export default function App() {
+  return (
+    <div>
+      {products.map((id, title, rating, price, imageurl) => (
+        <Product
+          key={id}
+          title={title}
+          imageUrl={imageurl}
+          price={price}
+          rating={rating}
+        />
+      ))}
+    </div>
+  );
+}
---
+++ components/Product.js
@@ -0,0 +1,25 @@
+import React from "react";
+const Product = ({ title, rating, price, imageUrl }) => {
+  return (
+    <div className="product">
+      <div className="product-image">
+        <img src={imageUrl} alt={title} />
+      </div>
+      <div className="title-container">
+        <h3>
+          <a href="#">{title}</a>
+        </h3>
+      </div>
+      <div>
+        <p>{rating.rate} ⭐️ ⭐️ ⭐️ ⭐️ ⭐️</p>
+        <p>{price}</p>
+      </div>
+      <div>
+        <button>Add to cart</button>
+        <button>Buy Now</button>
+      </div>
+    </div>
+  );
+};
+
+export default Product
