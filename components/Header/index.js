3256e18711cc6616e5d202393eb0b14b30e8826a
Abhignajonnalagadda <abhignajonnalagadda2580@gmail.com>
2025-08-30 11:13:02 +0530

    fix: show correct cart count

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,8 +1,12 @@
 import { Link } from "react-router-dom";
 import { FaCartShopping } from "react-icons/fa6";
 import "./index.css";
+import { use } from "react";
+import { useSelector } from "react-redux";
 
 const Header = () => {
+  const cart = useSelector((store) => store.cart);
+  const cartItemCount = cart.reduce((acc, item) => acc + item.quantity, 0);
   return (
     <>
       <header>
@@ -13,7 +17,7 @@ const Header = () => {
           <Link to="/cart" className="cart-link">
             <div className="cart">
               <FaCartShopping />
-              <p>0</p>
+              <p>{cartItemCount}</p>
             </div>
           </Link>
         </div>
